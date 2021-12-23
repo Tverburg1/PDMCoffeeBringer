d18b33a91879654e8d4f9be78abacf402ea8347c
tstienstra <T.J.Stienstra@student.tudelft.nl>
2021-12-23 12:04:03 +0100

    Add configuration publisher to monitor the current configuration

---
+++ youbot_motion_controller/src/youbot_configuration_publisher.cpp
@@ -0,0 +1,24 @@
+#include "youbot_motion_controller.hpp"
+
+
+int main(int argc, char *argv[]) {
+    ros::init(argc, argv, "youbot_configuration_publisher");
+    ros::NodeHandle node_handle("~");
+    ros::Publisher pub = node_handle.advertise<youbot_msgs::Control>("/youbot/current_configuration", 100);
+    YouBotMotionController ymc(node_handle);
+    ros::Rate rate(20);
+    while (ros::ok()) {
+        youbot_msgs::Control msg;
+        auto config = ymc.get_current_configuration();
+        msg.X = config(0);
+        msg.Y = config(1);
+        msg.theta = config(2);
+        msg.joint_1 = config(3);
+        msg.joint_2 = config(4);
+        msg.joint_3 = config(5);
+        msg.joint_4 = config(6);
+        msg.joint_5 = config(7);
+        pub.publish(msg);
+        rate.sleep();
+    }
+}
